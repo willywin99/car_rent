74eed1962b2fcc024bd749ba555e3b3e39c40acf
willywin99 <willywinsianturi@gmail.com>
2025-04-23 09:35:46 +0700

    Initial commit

---
+++ database/migrations/2025_04_23_022330_create_cars_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('car_name', 50)->nullable(false);
+            $table->double('double')->nullable(false);
+            $table->double('month_rate')->nullable(false);
+            $table->string('image', 256)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
